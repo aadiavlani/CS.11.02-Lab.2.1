d8b532880e9a9a51a3c56e8fc14a4e64c9678cea
aadiavlani <aadia2026@student.cis.edu.hk>
2024-03-04 15:11:51 +0800

    Unit 2 Lab V2

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,40 @@ public class Main {
 
     public static void main(String[] args) {
 
+        morningGreeting("zhongguo");
+
     }
 
     // 1. add
-    
+    public static int add(int a, int b)
+    {return a+b;}
     // 2. add
-
+    public static int add(int a, int b, int c, int d){
+        return add(a,b) + add(c,d) + add(0,0);
+    }
     // 3. morningGreeting
-
+    public static String morningGreeting(String urmom){
+        return "早上好, " + urmom + "!";
+    }
     // 4. afternoonGreeting
-
+    public static String afternoonGreeting(String urmom) {
+        return "下午好, " + urmom + "!";
+    }
     // 5. triple
-
+    public static String triple(String urmom) {
+        return urmom + urmom + urmom;
+    }
     // 6. half
-
+    public static double half(double a){
+        return a/2;
+    }
     // 7. roundPositiveValueToNearestInteger
+    public static int roundPositiveValueToNearestInteger(double a){
+        return (int) (a + 0.5);
+    }
 
-    // 8. roundNegativeValueToNearestInteger
+    public static int roundNegativeValueToNearestInteger(double a) {
+        return (int) (a - 0.5);
 
+    }
 }
